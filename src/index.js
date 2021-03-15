cdf2f28af7d908cccb8edb83200a76781069b127
avdeevam <avdeev.am471@gmail.com>
2021-03-16 00:45:57 +0300

    feat: add check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let bracketsMap = bracketsConfig.map((el) => el.join(""));
+    for (let i = 0; i < bracketsMap.length; i++) {
+        if (str.includes(bracketsMap[i])) {
+            str = str.replace(bracketsMap[i], "");
+            i = -1;
+        }
+    }
+    if (str) return false;
+    else return true;
+};
